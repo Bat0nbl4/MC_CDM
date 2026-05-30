a5869522a998b7837e90d975fb64e6ff10fbfa1a
Bat0nbl4 <Bat0nbl4@mail.ru>
2026-05-30 21:51:19 +0700

    Lite update view index.php

--- resources/render/views/index.php
+++ resources/render/views/index.php
@@ -1,2 +1,10 @@
 <?php \core\rendering\View::title(BASE_PAGE_NAME."Главная"); ?>
 
+<div class="container my-5 py-4">
+    <h1 class="text-center text-main">Твоя территория развития. Твой КДМ.</h1>
+    <p class="text-center">Кружки, мероприятия, волонтёрство и карьерные возможности для молодёжи края. Присоединяйся!</p>
+</div>
+
+<div class="container">
+    <h2 class="text-center">Наши мероприятия</h2>
+</div>
